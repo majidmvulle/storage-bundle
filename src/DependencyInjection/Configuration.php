16504cb58251cb82231516857c3b7cfba199942e
majidmvulle <majid@majidmvulle.com>
2018-11-07 23:16:22 +0400

    Secondies

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MajidMvulle\Bundle\UtilityBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('majidmvulle_utility')->addDefaultsIfNotSet();
+
+        $rootNode
+            ->children()
+                ->arrayNode('mailer')->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('from_email')->defaultValue('')->end()
+                        ->scalarNode('from_sender_name')->defaultValue('')->end()
+                    ->end()
+                ->end()//mailer
+                ->arrayNode('twilio')->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('sid')->defaultValue('')->end()
+                        ->scalarNode('token')->defaultValue('')->end()
+                        ->scalarNode('from_number')->defaultValue('')->end()
+                    ->end()
+                ->end()//twilio
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ src/DependencyInjection/MajidMvulleStorageExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MajidMvulle\Bundle\StorageBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class MajidMvulleStorageExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+
+    public function getAlias()
+    {
+        return 'majidmvulle_storage';
+    }
+}
